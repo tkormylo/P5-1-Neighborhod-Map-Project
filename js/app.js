fa03c23309f31af5e39c2278426e8900ac97129c
thomaskormylo <tkormylo@gmail.com>
2015-11-04 12:46:33 -0700

    Update app.js with ViewModel and update HTML with databinding
    Update the app.js file to have a ViewModel. Update the HTML to use the
    app.js ViewModel to show a list of locations contained in the Model.

--- js/app.js
+++ js/app.js
@@ -17,3 +17,24 @@ var locationModel = [
         lng: -116.200712
     }
 ];
+
+// Location object
+var Location = function (data) {
+    this.name = ko.observable(data.name);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+};
+
+// ViewModel
+var ViewModel = function () {
+    var self = this;
+
+    this.locationArray = ko.observableArray([]);
+
+    locationModel.forEach(function (locationItem) {
+        self.locationArray.push(new Location(locationItem));
+    });
+};
+
+// Initiate the KO bindings
+ko.applyBindings(new ViewModel());
