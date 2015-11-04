929953b337adf7e0e164860adef712e7059acc48
thomaskormylo <tkormylo@gmail.com>
2015-11-04 13:33:49 -0700

    Add map markers based on locations in data model
    Markers are now placed on the map based on locations in the model when
    the web app loads. Basic functionality working, needs more work.

--- js/app.js
+++ js/app.js
@@ -34,18 +34,33 @@ var ViewModel = function () {
     locationModel.forEach(function (locationItem) {
         self.locationArray.push(new Location(locationItem));
     });
-};
 
-// Initiate the KO bindings
-ko.applyBindings(new ViewModel());
+};
 
 // Google Maps API
 // Code to initialize the map when the web page loads
 // Obtained from: https://developers.google.com/maps/documentation/javascript/examples/map-simple
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+
+    var myLatLng = {lat: 43.614036, lng: -116.201171};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 43.618722, lng: -116.215768}, // Center map to Boise, ID
+        zoom: 15 // Zoom map to show downtown Boise, ID up close
+    });
+
+    // Place Google Map Markers on map
+    // Obtained from: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
+    locationModel.forEach(function (locationItem) {
+        var marker = new google.maps.Marker({
+            position: {lat: locationItem.lat, lng: locationItem.lng},
+            map: map,
+            title: 'Dynamically Created'
+        });
+    });
 }
+
+// Initiate the knockout.js bindings
+ko.applyBindings(new ViewModel());
+
