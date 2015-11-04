7f8d67c8072883ca16c4c0f8c0952ed23d328a51
thomaskormylo <tkormylo@gmail.com>
2015-11-04 12:38:14 -0700

    Update HTML and Add Model Code to app.js
    HTML has been updated to include necessary starting scripts and the
    app.js file has had the model data added.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Data Model
+// These are the locations that should show on the map when the web page loads.
+var locationModel = [
+    {
+        name: 'China Blue',
+        lat: 43.614036,
+        lng: -116.201171
+    },
+    {
+        name: 'Dirty Little Roddys',
+        lat: 43.614080,
+        lng: -116.200995
+    },
+    {
+        name: 'Pengillys Saloon',
+        lat: 43.614036,
+        lng: -116.200712
+    }
+];
