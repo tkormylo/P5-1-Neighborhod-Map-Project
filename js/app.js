9ec4cb0bf4ef7931bbd89781c315a8767050229a
thomaskormylo <tkormylo@gmail.com>
2015-11-05 08:03:42 -0700

    Add more bar locations - 15 total now - update map center
    Updated app to have 15 locations. Also updated the center of the map
    location on load and zoom level.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,73 @@ var locationModel = [
         marker: {}
     },
     {
-        name: 'Dirty Little Roddys',
+        name: 'Dirty Little Roddy\'s',
         location: {lat: 43.614080, lng: -116.200995},
         marker: {}
     },
     {
         name: 'Pengillys Saloon',
         location: {lat: 43.614036, lng: -116.200712},
+        marker: {}
+    },
+    {
+        name: 'Reef',
+        location: {lat: 43.614435, lng: -116.201461},
+        marker: {}
+    },
+    {
+        name: 'Hannah\'s',
+        location: {lat: 43.614718, lng: -116.202104},
+        marker: {}
+    },
+    {
+        name: 'Taphouse',
+        location: {lat: 43.615534, lng: -116.202754},
+        marker: {}
+    },
+    {
+        name: 'The Piper Pub & Grill',
+        location: {lat: 43.615701, lng: -116.202986},
+        marker: {}
+    },
+    {
+        name: 'Mulligans\' Pub & Eatery',
+        location: {lat: 43.616932, lng: -116.206382},
+        marker: {}
+    },
+    {
+        name: 'Pre Funk Beer Bar',
+        location: {lat: 43.616216, lng: -116.208503},
+        marker: {}
+    },
+    {
+        name: 'Solid Grill & Bar',
+        location: {lat: 43.613429, lng: -116.205984},
+        marker: {}
+    },
+    {
+        name: 'Liquid Lounge',
+        location: {lat: 43.613332, lng: -116.206378},
+        marker: {}
+    },
+    {
+        name: 'Double Tap Pub',
+        location: {lat: 43.613048, lng: -116.206335},
+        marker: {}
+    },
+    {
+        name: 'Bodovino',
+        location: {lat: 43.612941, lng: -116.205531},
+        marker: {}
+    },
+    {
+        name: 'Modern Bar',
+        location: {lat: 43.618391, lng: -116.209987},
+        marker: {}
+    },
+    {
+        name: 'Neurolux',
+        location: {lat: 43.618010, lng: -116.206264},
         marker: {}
     }
 ];
@@ -43,8 +103,8 @@ var map;
 function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 43.618722, lng: -116.215768}, // Center map to Boise, ID
-        zoom: 15 // Zoom map to show downtown Boise, ID up close
+        center: {lat: 43.615612, lng: -116.203751}, // Center map to Boise, ID
+        zoom: 16 // Zoom map to show downtown Boise, ID up close
     });
 
     // Place Google Map Markers on map
