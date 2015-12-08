97c54cdb936de053b21ad25e395767242f885dfc
thomaskormylo <tkormylo@gmail.com>
2015-12-07 21:40:04 -0700

    Added code to display foursquare api data in marker info window when selected from list of locations.

--- js/app.js
+++ js/app.js
@@ -4,84 +4,112 @@ var locationModel = [
     {
         name: 'China Blue',
         location: {lat: 43.614036, lng: -116.201171},
+        fourSquareVenueID: '4be0aae5b238d13a47a870a4',
         marker: {}
     },
     {
         name: 'Dirty Little Roddy\'s',
         location: {lat: 43.614080, lng: -116.200995},
+        fourSquareVenueID: '4b45bfb7f964a520d00f26e3',
         marker: {}
     },
     {
         name: 'Pengilly\'s Saloon',
         location: {lat: 43.614036, lng: -116.200712},
+        fourSquareVenueID: '4b47ca74f964a520dc3e26e3',
         marker: {}
     },
     {
         name: 'Reef',
         location: {lat: 43.614435, lng: -116.201461},
+        fourSquareVenueID: '4b4781c6f964a5204a3426e3',
         marker: {}
     },
     {
         name: 'Hannah\'s',
         location: {lat: 43.614718, lng: -116.202104},
+        fourSquareVenueID: '4b45c054f964a520d70f26e3',
         marker: {}
     },
     {
         name: 'Taphouse',
         location: {lat: 43.615534, lng: -116.202754},
+        fourSquareVenueID: '4f061f2177c8f87cd21c87f3',
         marker: {}
     },
     {
         name: 'The Piper Pub & Grill',
         location: {lat: 43.615701, lng: -116.202986},
+        fourSquareVenueID: '4b4519f6f964a520a40426e3',
         marker: {}
     },
     {
         name: 'Mulligans\' Pub & Eatery',
         location: {lat: 43.616932, lng: -116.206382},
+        fourSquareVenueID: '4b45c338f964a520f80f26e3',
         marker: {}
     },
     {
         name: 'Pre Funk Beer Bar',
         location: {lat: 43.616216, lng: -116.208503},
+        fourSquareVenueID: '51f6d2c7498eeefd09925f7c',
         marker: {}
     },
     {
         name: 'Solid Grill & Bar',
         location: {lat: 43.613429, lng: -116.205984},
+        fourSquareVenueID: '4b7756c2f964a5204e932ee3',
         marker: {}
     },
     {
         name: 'Liquid Lounge',
         location: {lat: 43.613332, lng: -116.206378},
+        fourSquareVenueID: '4b462a6af964a520c31826e3',
         marker: {}
     },
     {
-        name: 'Double Tap Pub',
-        location: {lat: 43.613048, lng: -116.206335},
+        name: 'The Matador',
+        location: {lat: 43.617241, lng: -116.202416},
+        fourSquareVenueID: '4c65720118b676b09317aa0e',
         marker: {}
     },
     {
         name: 'Bodovino',
         location: {lat: 43.612941, lng: -116.205531},
+        fourSquareVenueID: '514b67b7f2e738cb0d22597b',
         marker: {}
     },
     {
         name: 'Modern Bar',
         location: {lat: 43.618391, lng: -116.209987},
+        fourSquareVenueID: '4b496efaf964a520bd6f26e3',
         marker: {}
     },
     {
         name: 'Neurolux',
         location: {lat: 43.618010, lng: -116.206264},
+        fourSquareVenueID: '4b43d6b2f964a5209ceb25e3',
         marker: {}
     }
 ];
+
+var map;
+var infoWindowContentString = '';
+
+var fsVenueID = '';
+var fsClientID = '4V5DEEIKDI0JBNNO4KJK03BEADIOKLRLDENVJTNBAYAIQQFY';
+var fsClientSecret = 'CQ21XTFDMK4QSIRHDY2N5SN5LJPV5HHCDAVZKI3OBFJDQFS0';
+var fsVersionDate = '20161231';
+var fsMode = 'foursquare';
+var fsAPICallSettings = {};
+
+var fsResult = {};
 
 // Location object
 var Location = function (data) {
     this.name = ko.observable(data.name);
     this.location = ko.observable(data.location);
+    this.fourSquareVenueID = ko.observable(data.fourSquareVenueID);
     this.marker = ko.observable({});
 };
 
@@ -108,7 +136,6 @@ var ViewModel = function () {
 
         for(var x in locationModel) {
             if(locationModel[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-                //self.locationArray.push(locationModel[x]);
                 self.locationArray.push(new Location(locationModel[x]));
             }
         }
@@ -128,52 +155,66 @@ var ViewModel = function () {
     this.query.subscribe(this.search);
 };
 
-var fsClientID = '4V5DEEIKDI0JBNNO4KJK03BEADIOKLRLDENVJTNBAYAIQQFY';
-var fsClientSecret = 'CQ21XTFDMK4QSIRHDY2N5SN5LJPV5HHCDAVZKI3OBFJDQFS0';
-var fsVersionDate = '20161231';
-var fsMode = 'foursquare';
-
-var fsAPICallSettings = {
-    url: 'https://api.foursquare.com/v2/venues/4b4781c6f964a5204a3426e3?client_id=' + fsClientID + '&client_secret=' + fsClientSecret + '&v=' + fsVersionDate + '&m=' + fsMode,
-    dataType: 'json',
-    success: function() {
-        console.log('Got a file');
-    },
-    error: function() {
-        console.log('Error getting file');
-    }
-}
-
 // Code to execute when a list item (location) is clicked
 $('#location-list').on('click', 'li', function() {
-
-    // Add code here to perform foursquare API call
-    $.ajax(fsAPICallSettings);
+    // Get the location name of the list item selected
+    var selectedItemName = $(this).text();
 
     // Add / Remove the "highlight" class to highlight the selected item
     $('.highlight').removeClass('highlight');
     $(this).toggleClass('highlight');
 
-    // Get the location name of the list item selected
-    var selectedItemName = $(this).text();
-
     // Scan the location array and bounce the marker of the location with a matching name of the clicked list item.
     appVM.locationArray().forEach(function (locationItem) {
         if(locationItem.name() == selectedItemName) {
+            // Bounce the selected location marker
             toggleBounce(locationItem.marker);
+
             // Close all open marker windows
             closeMarkerInfoWindows();
-            // Open the marker info window
-            openMarkerInfoWindow(locationItem.marker);
+
+            // Update the foursquare venue ID with the location selected from the list
+            fsVenueID = locationItem.fourSquareVenueID();
+
+            // Perform foursquare API call
+            var url = 'https://api.foursquare.com/v2/venues/' + fsVenueID + '?client_id=' + fsClientID + '&client_secret=' + fsClientSecret + '&v=' + fsVersionDate + '&m=' + fsMode
+
+            $.getJSON(url, function (json) {
+
+                //Update marker infowindow with content to display
+                locationItem.marker.infowindow.setContent('<div class="container-fluid">' +
+                    '<div class="row">' +
+                        '<div class="col-md-12">' +
+                            '<h3>' + json.response.venue.name +'</h3>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<div class="col-md-6">' +
+                            '<address>' +
+                                json.response.venue.location.address + '<br>' +
+                                '<abbr title="Phone">P:</abbr>' + json.response.venue.contact.formattedPhone +
+                            '</address>' +
+                            '<p>' + json.response.venue.hours.status + '</p>' +
+                            '<a target="_blank" href="' + json.response.venue.canonicalUrl + '">Show My Foursquare Web Page!</a>' +
+                        '</div>' +
+                        '<div class="col-md-6">' +
+                            '<img src="' + json.response.venue.bestPhoto.prefix + '125x125' + json.response.venue.bestPhoto.suffix + '" class="img-responsive">' +
+                        '</div>' +
+                    '</div>');
+
+                // Open the marker info window
+                openMarkerInfoWindow(locationItem.marker);
+            });
         };
     });
 });
 
+
+
+
 // Google Maps API
 // Code to initialize the map when the web page loads
 // Obtained from: https://developers.google.com/maps/documentation/javascript/examples/map-simple
-var map;
-var infoWindowContentString = '';
 function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
